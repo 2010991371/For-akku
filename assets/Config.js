94b1fab1d8cc5fedb2adbafebd1ec759e95bb972
Kapish <kapish1371.be20@chitkara.edu.in>
2026-07-21 23:38:47 +0530

    Create Config.js

---
+++ assets/Config.js
@@ -0,0 +1,71 @@
+const WEBSITE_CONFIG = {
+
+name: "Aku",
+
+from: "Aditya",
+
+music: "piano.mp3",
+
+photo: "our-photo.jpg",
+
+theme: {
+
+primary: "#ffffff",
+
+accent: "#ff4d6d",
+
+background: "#000000"
+
+},
+
+story: [
+
+{
+
+title: "Some people don't just enter your life...",
+
+text: "They become your home."
+
+},
+
+{
+
+title: "Aku...",
+
+text: "I've replayed that day in my mind more times than I can count."
+
+},
+
+{
+
+title: "We fought badly.",
+
+text: "I let my anger speak louder than my love."
+
+},
+
+{
+
+title: "I'm not asking you to forget.",
+
+text: "I'm asking for one chance to become better."
+
+},
+
+{
+
+title: "Every smile we've shared...",
+
+text: "is a memory I never want to lose."
+
+},
+
+{
+
+title: "ALWAYS & FOREVER ❤️",
+
+text: "— Aditya"
+
+}
+
+];
